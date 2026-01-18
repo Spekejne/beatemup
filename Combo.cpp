b1641a1e06165ea64fff6ad4b6a0b1afa4a05f09
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-18 23:47:04 +0100

    Update Combo.cpp

--- Combo.cpp
+++ Combo.cpp
@@ -1,14 +1,16 @@
+// combo.cpp
 #include "Combo.h"
 
 void initCombo(ComboBuffer* cb) {
     cb->index = 0;
-    for (int i = 0; i < INPUT_BUFFER; i++) cb->buffer[i] = IN_NONE;
+    for (int i = 0; i < INPUT_BUFFER; i++) {
+        cb->buffer[i] = IN_NONE;
+        cb->time[i] = 0.0f;
+    }
 }
 
 void pushInput(ComboBuffer* cb, InputType in, float t) {
     cb->buffer[cb->index] = in;
-    cb->time[cb->index] = t;   // ← skoro masz time[] w struct
+    cb->time[cb->index] = t;
     cb->index = (cb->index + 1) % INPUT_BUFFER;
 }
-
-
