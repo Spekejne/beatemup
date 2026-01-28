20b821bfa56c7cb210bd421689e6e2dd185581d8
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-28 21:26:19 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -17,33 +17,50 @@ Enemy enemies[16];
 int enemyCount = 0;
 
 bool initGame() {
-    char* basePath = SDL_GetBasePath();
-printf("Base path: %s\n", basePath);
-SDL_free(basePath);
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) return false;
-
-    SDL_Surface* surface = SDL_LoadBMP("background.bmp");
-    if (!surface) {
-    printf("Nie mozna zaladowac background.bmp: %s\n", SDL_GetError());
-    return false;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL init error: %s\n", SDL_GetError());
+        return false;
     }
 
+    // 1️⃣ OKNO
+    window = SDL_CreateWindow(
+        "Beat'em Up",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        0
+    );
+    if (!window) {
+        printf("Window error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // 2️⃣ RENDERER (MUSI BYĆ TU)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // 3️⃣ DOPIERO TERAZ BITMAPA
+    char* basePath = SDL_GetBasePath();
+    char path[512];
+    snprintf(path, sizeof(path), "%sbackground.bmp", basePath);
+    SDL_free(basePath);
+
+    SDL_Surface* surface = SDL_LoadBMP(path);
+    if (!surface) {
+        printf("BMP load error: %s\n", SDL_GetError());
+        return false;
+    }
 
     background = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
-
     if (!background) {
-    printf("Nie mozna utworzyc tekstury tla: %s\n", SDL_GetError());
-    return false;
+        printf("Texture error: %s\n", SDL_GetError());
+        return false;
     }
-
-    window = SDL_CreateWindow("Beat'em Up",
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              800, 600, 0);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     initPlayer(&player);
     loadLevel("level.txt", enemies, &enemyCount);
@@ -80,6 +97,7 @@ void gameLoop() {
         SDL_Delay(16);
     }
 }
+
 
 
 
