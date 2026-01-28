f794860c830b8f5177bc647c9c5f2a56927b5a7b
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-28 20:58:43 +0100

    Update Render.cpp

--- Render.cpp
+++ Render.cpp
@@ -2,11 +2,16 @@
 #include "Game.h"
 
 void renderFrame(Player* p, Enemy* e, int count) {
-    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
     SDL_RenderClear(renderer);
 
+    // 🔹 TŁO
+    SDL_Rect bg = { 0, 0, 800, 600 };
+    SDL_RenderCopy(renderer, background, NULL, &bg);
+
+    // 🔹 GRACZ
     drawAnimation(&p->idle, p->x, p->y - p->z, p->facing);
 
+    // 🔹 WROGOWIE
     for (int i = 0; i < count; i++)
         if (e[i].alive)
             drawAnimation(&e[i].idle, e[i].x, e[i].y, 1);
