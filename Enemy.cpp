1bc5c1f279bd1e128b124e5dc92c9f8b90e1b022
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 00:04:27 +0100

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,5 +1,6 @@
 #include "Enemy.h"
 #include "Game.h"
+#include <cmath>
 
 void initEnemy(Enemy* e, float x, float y) {
     e->x = x;
@@ -28,3 +29,29 @@ void initEnemy(Enemy* e, float x, float y) {
     );
 }
 
+void updateEnemy(Enemy* e, Player* p, float dt) {
+if (!e->alive) return;
+
+
+float dx = p->x - e->x;
+
+
+if (fabs(dx) > 5) {
+e->vx = (dx > 0) ? 80 : -80;
+e->facing = (dx > 0) ? 1 : -1;
+e->action = EN_WALK;
+} else {
+e->vx = 0;
+e->action = EN_IDLE;
+}
+
+
+e->x += e->vx * dt;
+
+
+// 🔹 animacje
+if (e->action == EN_WALK)
+updateAnimation(&e->walk, dt);
+else
+updateAnimation(&e->idle, dt);
+}
