b3418d2ebeca3571db20941b2c16898a1afef385
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-28 22:38:36 +0100

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -5,25 +5,32 @@
 void handleInput(Player* p) {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) running = 0;
+        if (e.type == SDL_QUIT)
+            running = 0;
     }
 
     const Uint8* k = SDL_GetKeyboardState(nullptr);
-    p->vx = 0;
-    p->action = ACT_IDLE;
 
-    if (k[SDL_SCANCODE_A]) { p->vx = -200; p->facing = -1; p->action = ACT_WALK; }
-    if (k[SDL_SCANCODE_D]) { p->vx = 200;  p->facing = 1;  p->action = ACT_WALK; }
+    // input tylko steruje ruchem
+    p->vx = 0;
+
+    if (k[SDL_SCANCODE_A]) {
+        p->vx = -200;
+        p->facing = -1;
+    }
+    else if (k[SDL_SCANCODE_D]) {
+        p->vx = 200;
+        p->facing = 1;
+    }
 
     if (k[SDL_SCANCODE_SPACE] && p->z == 0) {
         p->vz = 20;
-        p->action = ACT_JUMP;
     }
 
     if (k[SDL_SCANCODE_X]) {
-        p->action = ACT_ATTACK;
         pushInput(&p->combo, IN_X, SDL_GetTicks());
     }
 }
+
 
 
