e00bf42e5764c895826f20be652052df33e492ac
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-28 20:58:20 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -15,6 +15,23 @@ Player player;
 Enemy enemies[16];
 int enemyCount = 0;
 
+#include "Game.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "Input.h"
+#include "Combat.h"
+#include "Render.h"
+#include "level.h"
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+int running = 1;
+GameState gameState = STATE_MENU;
+
+Player player;
+Enemy enemies[16];
+int enemyCount = 0;
+
 bool initGame() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) return false;
 
@@ -76,5 +93,38 @@ void gameLoop() {
         SDL_Delay(16);
     }
 }
+
+
+
+void shutdownGame() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void gameLoop() {
+    Uint32 last = SDL_GetTicks();
+
+    while (running) {
+        Uint32 now = SDL_GetTicks();
+        float dt = (now - last) / 1000.0f;
+        last = now;
+
+        handleInput(&player);
+
+        if (gameState == STATE_GAME) {
+            updatePlayer(&player, dt);
+
+            for (int i = 0; i < enemyCount; i++)
+                updateEnemy(&enemies[i], &player, dt);
+
+            handleCombat(&player, enemies, enemyCount);
+        }
+
+        renderFrame(&player, enemies, enemyCount);
+        SDL_Delay(16);
+    }
+}
+
 
 
