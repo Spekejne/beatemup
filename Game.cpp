1368a3f0e2a70175ea9cd24df445543ebe388182
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 13:28:32 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -135,10 +135,11 @@ handleCombat(&player, enemies, enemyCount);
         renderFrame(&player, enemies, enemyCount);
         SDL_Delay(16);
     }
-if gameState == STATE_EXIT{
+if (gameState == STATE_EXIT){
     shutdownGame();
 }
 }
+
 
 
 
