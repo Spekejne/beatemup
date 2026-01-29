9b74585804de5f5dc79e4b764808d411a486153f
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 10:40:54 +0100

    Create Collision.cpp

---
+++ Collision.cpp
@@ -0,0 +1,14 @@
+#include "Collision.h"
+
+void resolveBodyCollision(Player* p, Enemy* e) {
+    if (!intersects(p->hurtbox, e->hurtbox))
+        return;
+
+    float overlapLeft  = (p->hurtbox.x + p->hurtbox.w) - e->hurtbox.x;
+    float overlapRight = (e->hurtbox.x + e->hurtbox.w) - p->hurtbox.x;
+
+    float push = (overlapLeft < overlapRight) ? overlapLeft : -overlapRight;
+
+    p->x -= push * 0.5f;
+    e->x += push * 0.5f;
+}
