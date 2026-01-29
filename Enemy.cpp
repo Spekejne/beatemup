764400adb0d3849dec9ee20fe8720f6ca6d2e241
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 08:38:11 +0100

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,31 +2,22 @@
 #include "Game.h"
 #include <cmath>
 
+
 void initEnemy(Enemy* e, float x, float y) {
-    e->x = x;
-    e->y = y;
-    e->vx = 0;
-    e->facing = -1;
-    e->alive = 1;
-    e->action = EN_IDLE;
+e->x = x;
+e->y = y;
+e->vx = 0;
+e->facing = -1;
+e->alive = 1;
+e->hp = 3;
+e->action = EN_IDLE;
 
-    initAnimation(
-        &e->idle,
-        renderer,
-        "enemy_idle.bmp",
-        4,        // liczba klatek
-        24, 32,
-        0.15f
-    );
 
-    initAnimation(
-        &e->walk,
-        renderer,
-        "enemy_walk.bmp",
-        6,
-        24, 32,
-        0.1f
-    );
+initAnimation(&e->idle, renderer, "enemy_idle.bmp", 4, 24, 32, 0.2f);
+initAnimation(&e->walk, renderer, "enemy_walk.bmp", 6, 24, 32, 0.1f);
+initAnimation(&e->attack, renderer, "enemy_attack.bmp", 4, 32, 32, 0.1f);
+initAnimation(&e->hit, renderer, "enemy_hit.bmp", 2, 24, 32, 0.15f);
+initAnimation(&e->dead, renderer, "enemy_dead.bmp", 4, 24, 32, 0.2f);
 }
 
 void updateEnemy(Enemy* e, Player* p, float dt) {
@@ -55,3 +46,4 @@ updateAnimation(&e->walk, dt);
 else
 updateAnimation(&e->idle, dt);
 }
+
