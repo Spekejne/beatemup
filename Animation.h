0ba1cc28d2bfd555bf34e385fff2bf422c48e128
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 09:37:16 +0100

    Update Animation.h

--- Animation.h
+++ Animation.h
@@ -7,9 +7,11 @@ struct Animation {
     int w, h;
     float time, timer;
     int frame;
+    Uint8 rKey, Uint8 gKey, Uint8 bKey;
 };
 
 void initAnimation(Animation* a, SDL_Renderer* r,
                    const char* path, int f, int w, int h, float t);
 void updateAnimation(Animation* a, float dt);
 void drawAnimation(Animation* a, int x, int y, int flip);
+
