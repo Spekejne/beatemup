871898edd12ef7b3180e82f2210cd11e65f0d30f
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 09:13:24 +0100

    Update level.cpp

--- level.cpp
+++ level.cpp
@@ -4,14 +4,24 @@
 
 void loadLevel(const char* path, Enemy* enemies, int* count) {
     std::ifstream f(path);
+    if (!f.is_open()) {
+        printf("❌ NIE MOZNA OTWORZYC level.txt\n");
+        return;
+    }
+
     std::string type;
     float x, y;
     *count = 0;
 
     while (f >> type >> x >> y) {
+        printf("READ: %s %f %f\n", type.c_str(), x, y);
+
         if (type == "enemy" && *count < 16) {
             initEnemy(&enemies[*count], x, y);
             (*count)++;
         }
     }
+
+    printf("ENEMIES LOADED: %d\n", *count);
 }
+
