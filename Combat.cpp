75a50dfc0aaec57bcca951d1732985fe76de03fa
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-29 08:39:31 +0100

    Update Combat.cpp

--- Combat.cpp
+++ Combat.cpp
@@ -11,4 +11,19 @@ void handleCombat(Player* p, Enemy* e, int count) {
             if (e[i].hp <= 0) e[i].alive = 0;
         }
     }
+
+    if (player.action == ACT_ATTACK) {
+    for (int i = 0; i < enemyCount; i++) {
+        if (!enemies[i].alive) continue;
+
+        float dx = fabs(player.x - enemies[i].x);
+        if (dx < 40 && enemies[i].action != EN_HIT) {
+            enemies[i].hp--;
+            enemies[i].action = EN_HIT;
+            enemies[i].hit.frame = 0;
+            enemies[i].hit.timer = 0;
+        }
+    }
 }
+}
+
