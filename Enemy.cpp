139bb94753224fb40c10671217c8a7632fd31559
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-02-15 14:03:08 +0100

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -8,6 +8,7 @@ void initEnemy(Enemy* e, float x, float y) {
     e->x = x;
     e->y = y;
     e->vx = 0;
+    e->vy = 0;
     e->facing = -1;
     e->alive = 1;
     e->hp = 3;
@@ -58,12 +59,15 @@ void updateEnemy(Enemy* e, Player* p, float dt) {
         return;
 
     float dx = p->x - e->x;
+    float dy = p->y - e->y;
+
     float dist = fabs(dx);
+    flost distY = fabs(dy);
 
     e->facing = (dx > 0) ? 1 : -1;
 
     // ⚔️ ATAK – TYLKO BARDZO BLISKO
-    if (dist < 28) {
+    if (dist < 28 && distY < 20) {
         e->vx = 0;
         e->action = EN_ATTACK;
         updateAnimation(&e->attack, dt);
@@ -83,21 +87,30 @@ void updateEnemy(Enemy* e, Player* p, float dt) {
 }
 
     // 🚶 CHODZENIE DO GRACZA
+    e->vx = 0;
+    e->vy = 0;
+    
     if (dist > 30) {
         e->vx = (dx > 0) ? 60.0f : -60.0f;
-        e->action = EN_WALK;
-    } else {
-        e->vx = 0;
+    }
+    if (distY > 5)
+        e->vy = (dy > 0) ? 60.0f : -60.0f;
+
+    if (e->vx != 0 || e->vy != 0)
+        e->action = EN_WALK
+    else {
         e->action = EN_IDLE;
     }
 
     e->x += e->vx * dt;
+    e->y += e->vy * dt;
 
     if (e->action == EN_WALK)
         updateAnimation(&e->walk, dt);
     else
         updateAnimation(&e->idle, dt);
 }
+
 
 
 
